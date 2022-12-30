c2377c28a67538904d7917be2fd882bf7737d6e7
msemerni <msemerni@gmail.com>
2022-12-30 23:23:08 +0200

    branch dev add

---
+++ src/socketIO.ts
@@ -0,0 +1,21 @@
+// const io = require('socket.io')(http);
+
+// io.on('connection', (socket: any) => {
+//   console.log(`User connected: ${socket.id}`);
+
+//   socket.on('disconnect', function () {
+//     console.log(`User disconnected: ${socket.id}`);
+//   });
+
+//   socket.on('create game', (gameLink: any) => {
+//     const token = gameLink.slice(gameLink.lastIndexOf('/') + 1);
+//     const [string, iv ] = token.split(":");
+//     const decodedToken = JSON.parse(decrypt(string, iv));
+//     const roomGameID = decodedToken.gameUUID;
+
+//     socket.join(roomGameID);
+//     io.to(roomGameID).emit('game created', decodedToken.initiatorUser.login, roomGameID);
+//   })
+// });
+
+// export {io};
